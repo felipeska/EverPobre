a8895dfc13e77de73b1ad47538a554282aac201c
Hawer Torres <htorres@widetech.com.co>
2013-05-09 14:15:13 -0500

    Initial Commit

---
+++ EverPobre/WTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTAppDelegate.h
+//  EverPobre
+//
+//  Created by Hawer Torres on 9/05/13.
+//  Copyright (c) 2013 Widetech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EverPobre/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EverPobre
+//
+//  Created by Hawer Torres on 9/05/13.
+//  Copyright (c) 2013 Widetech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WTAppDelegate class]));
+    }
+}
